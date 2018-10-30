691136562d6a0db6671471e6e695b8127a46092a
Botafogo1894 <pruchka@gmail.com>
2018-10-30 18:29:07 -0400

    almost done

---
+++ dilyan_sol.py
@@ -0,0 +1,19 @@
+from car import Car
+from person import Person
+#all people
+
+jim = Person("Jim Halpert", "Paper Salesperson")
+stanley = Person("Stanley Hudson", "Paper Salesperson")
+pam = Person("Pam Beasley", "Secretary")
+michael = Person("Michael Scott", "Regional Manager")
+dwight = Person("Dwight Schrute", "Paper Salesperson")
+meredith = Person("Meredith Palmer", "Purchaser - Supplier Relations")
+
+#all cars
+
+sebring = Car("Chrysler", "Sebring Convertible", 2004, michael)
+datsun = Car("Datsun", "280Z", 1978, dwight)
+corolla = Car("Toyota", "Corolla", 2000, jim)
+toyota = Car("Toyota", "Yaris", 2007, pam)
+camry = Car ("Toyota", "Camry", 2008, stanley)
+ford_minivan = Car("Ford", "Aerostar Minivan", 1997, meredith)
--- person.py
+++ person.py
@@ -6,3 +6,24 @@ class Person:
     def __init__(self, name, occupation):
         self._name = name
         self._occupation = occupation
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def occupation(self):
+        return self._occupation
+
+    @classmethod
+    def has_oldest_car(cls):
+        oldest_year = min(list(map(lambda item: item.year, Car._all)))
+        return list(filter(lambda item: item.year == oldest_year, Car._all))[0].owner.name
+
+    @classmethod
+    def drives_a(cls, type):
+        empty = []
+        for item in Car._all:
+            if item.make == type:
+                empty.append(item.owner.name)
+        return empty
